d3c3a78d5d1bb0ecdc1dce13c7c1b72693ab7f97
Pranav Sant <santpranav1@gmail.com>
2024-07-25 16:00:55 -0400

    added more EDA

--- PranavProject/Scripts/dataCleaning.R
+++ PranavProject/Scripts/dataCleaning.R
@@ -113,3 +113,96 @@ clean_data$per_capita_gdp
 
 #####
 
+clean_data$population_millions
+
+
+clean_data <- mutate(clean_data, gdp = per_capita_gdp * as.numeric(population_millions))
+
+clean_data$gdp
+
+ggplot(data = clean_data, aes(x = per_capita_gdp, y = total_biocapacity, color = hdi)) +
+  geom_point() +
+  labs(title = "GDP Per Capita vs Total Biocapacity by HDI",
+       x = "GDP Per Capita",
+       y = "Biocapacity")
+
+ggplot(data = clean_data, aes(x = per_capita_gdp, y = total_biocapacity, color = hdi)) +
+  geom_point() +
+  labs(title = "GDP Per Capita vs Total Biocapacity by HDI",
+       x = "GDP Per Capita",
+       y = "Biocapacity")
+
+ggplot(data = clean_data, aes(x = per_capita_gdp, y = ecological_deficit_or_reserve, color = hdi)) +
+  geom_point() +
+  labs(title = "GDP Per Capita vs Ecological Deficit or Reserve by HDI",
+       x = "GDP Per Capita",
+       y = "Eco Deficit or Reserve") +
+  geom_smooth(color = "red")
+
+ggplot(data = clean_data, aes(x = per_capita_gdp, y = number_of_earths_required, color = hdi)) +
+  geom_point() +
+  labs(title = "GDP Per Capita vs Number of Earths Required by HDI",
+       x = "GDP Per Capita",
+       y = "Earths Required") +
+  geom_smooth(color = "red")
+
+
+## had to filter out US because its GDP is outlier
+# gdp_data <- filter(clean_data, gdp < 20000000)
+
+# Comparing GDP to Earths Required ####
+
+ggplot(data = gdp_data, aes(x = gdp, y = "")) +
+  geom_boxplot()
+
+quantile(clean_data$gdp, probs = c(0.25, 0.5, 0.75), na.rm = TRUE)
+
+
+grouped_data <- clean_data %>%
+  mutate(group = case_when(
+    gdp < 27885 ~ 1,
+    gdp > 27885 & gdp < 100136.2 ~ 2,
+    gdp > 100136.2 & gdp < 438837.8 ~ 3,
+    gdp > 438837.8 ~ 4
+  ))
+
+grouped_data$group
+
+group1 <- subset(grouped_data, group == 1)
+group2 <- subset(grouped_data, group == 2)
+group3 <- subset(grouped_data, group == 3)
+group4 <- subset(grouped_data, group == 4)
+
+ggplot(data = group1, aes(x = gdp, y = number_of_earths_required, color = sd_gi)) +
+  geom_point() +
+  labs(title = "Group 1 GDP vs Number of Earths Required",
+       x = "GDP (in millions)",
+       y = "Number of Earths Requried")
+
+ggplot(data = group2, aes(x = gdp, y = number_of_earths_required, color = sd_gi)) +
+  geom_point() +
+  labs(title = "Group 2 GDP vs Number of Earths Required",
+       x = "GDP (in millions)",
+       y = "Number of Earths Requried")
+
+ggplot(data = group3, aes(x = gdp, y = number_of_earths_required, color = sd_gi)) +
+  geom_point() +
+  labs(title = "Group 3 GDP vs Number of Earths Required",
+       x = "GDP (in millions)",
+       y = "Number of Earths Requried")
+
+ggplot(data = group4, aes(x = gdp, y = number_of_earths_required, color = sd_gi)) +
+  geom_point() +
+  labs(title = "Group 4 GDP vs Number of Earths Required",
+       x = "GDP (in millions)",
+       y = "Number of Earths Requried")
+
+
+
+
+ggplot(data = gdp_data, aes(x = gdp, y = number_of_earths_required, color = hdi)) +
+  geom_point() +
+  labs(title = "GDP vs Number of Earths Required by HDI",
+       x = "GDP",
+       y = "Earths Required") +
+  geom_smooth(color = "red")
